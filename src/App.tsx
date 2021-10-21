62e012d5e1d08bca8ff3bd6736b3a274443b83b5
Nick LaMonica <nila2238@colorado.edu>
2021-10-20 18:39:46 -0600

    layout css for landing page

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <h1>Nick LaMonica</h1>
       </header>
+      <div className="links-wrapper">
+        <div className="links">
+            <div className="link">Github</div>
+            <div className="link">linkedin</div>
+            <div className="link">Twitter</div>
+            <div className="link">resume</div>
+        </div>
+      </div>
     </div>
   );
 }
