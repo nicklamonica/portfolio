517613faa2978f3931a8839a2953aec9d35a9086
Nick LaMonica <nila2238@colorado.edu>
2022-03-13 20:24:47 -0600

    add chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
       <h1>Nick LaMonica</h1>
       </header>
-      <div className="links-wrapper">
-        <div className="links">
-            <div className="link">Github</div>
-            <div className="link">linkedin</div>
-            <div className="link">Twitter</div>
-            <div className="link">resume</div>
+      <div >
+        <div>
+            <div >Github</div>
+            <div >linkedin</div>
+            <div>Twitter</div>
+            <div>resume</div>
         </div>
       </div>
     </div>
